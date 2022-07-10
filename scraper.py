f1032b7d9655eec2c8da720a5c024dffbea388b1
JohnSolutions <jonhsmithd930@gmail.com>
2022-07-10 17:36:52 -0300

    Updated scraper.py
    Wrote functions to get all the links from Wikipedia.

--- scraper.py
+++ scraper.py
@@ -7,10 +7,37 @@ import json
 # Global variables:
 
 parent_url = 'https://en.wikipedia.org/wiki/Category:Lists_of_high_schools_in_the_United_States_by_state'
-base_url = 'https://en.wikipedia.org/'
-states_url_list = []
-
+base_url = 'https://en.wikipedia.org'
 
 def get_state_urls(url):
 
-    request = 
+    request = requests.get(parent_url)
+    soup = BeautifulSoup(request.text, 'html.parser')
+
+    state_lists = soup.find_all('div', class_ = 'mw-category-group')
+
+    states_url_list = []
+
+    for list in state_lists:
+
+        states = list.find_all('a')
+
+        for state in states:
+
+            href = state['href']
+            link = base_url + href
+
+            states_url_list.append(link)
+
+    return states_url_list
+
+
+def get_schools(states_urls):
+
+    for url in states_urls:
+
+        request = requests.get(url)
+        soup = BeautifulSoup(request.text, 'html.parser')
+
+        
+
