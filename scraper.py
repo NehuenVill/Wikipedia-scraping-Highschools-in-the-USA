9f7541748d002c21364400f44b5b8c6cc5874470
JohnSolutions <jonhsmithd930@gmail.com>
2022-07-10 12:58:49 -0300

    Updated scraper.py
    Added global variables and figured out the work-flow of the scraper program.

--- scraper.py
+++ scraper.py
@@ -1,3 +1,16 @@
 from bs4 import BeautifulSoup
 import pandas as pd
+import requests
+import time
+import json
 
+# Global variables:
+
+parent_url = 'https://en.wikipedia.org/wiki/Category:Lists_of_high_schools_in_the_United_States_by_state'
+base_url = 'https://en.wikipedia.org/'
+states_url_list = []
+
+
+def get_state_urls(url):
+
+    request = 
