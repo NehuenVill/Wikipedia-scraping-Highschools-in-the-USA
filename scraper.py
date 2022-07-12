f379c0cef224ced2058c6deacf007e36d3642e90
JohnSolutions <jonhsmithd930@gmail.com>
2022-07-12 15:38:26 -0300

    Added new text.txt
    Made changes to functions and added text file to search for RegEx.

--- scraper.py
+++ scraper.py
@@ -3,6 +3,7 @@ import pandas as pd
 import requests
 import time
 import json
+import re
 
 # Global variables:
 
@@ -34,10 +35,123 @@ def get_state_urls(url):
 
 def get_schools(states_urls):
 
+    all_schools = []
+
+    cleaning_pattern = r'- [a-z ]{20,10000},? [a-z ]*|\([a-z ]*\)'
+
     for url in states_urls:
 
         request = requests.get(url)
         soup = BeautifulSoup(request.text, 'html.parser')
 
-        
+        unuseful_links = soup.find_all('link')
+
+        split_url = url.split('_')
+        state_name = split_url[len(split_url) - 1]
+
+
+        print('*'*50, state_name, '*'*50)
+
+        for i in unuseful_links:
+
+            i.decompose()
+
+        counties = soup.find_all('h2')
+
+        for county in counties:
+
+            if 'County' not in county.text and 'Ward' not in county.text:
+
+                pass
+
+            else:
+
+                print('*'*50, county.text, '*'*50)
+
+                time.sleep(0.05)
+
+                next_element = county.find_next_sibling()
+
+                city = ''
+
+                while True:
+
+                    if next_element.name == 'div' or next_element.name == 'ul':
+
+                        if next_element['class'][0] == 'hatnote navigation-not-searchable':
+
+                            pass
+
+                        print('next element: ', next_element.name)
+
+                        time.sleep(0.05)
+
+                        school_list = next_element.find_all('li')
+                        schools = [school.text for school in school_list]
+
+                        for school in schools:
+
+                            if ' - ' in school:
+
+                                school = re.sub(cleaning_pattern, '', school)
+
+                            if ', ' in school:
+
+                                school_info = school.split(', ')
+                                name = school_info[0]
+                                city = school_info[1]
+
+                                school_output = {
+                                    'School name': name, 
+                                    'State': state_name,
+                                    'County': county.text,
+                                    'City/town/village/District': city,
+                                }
+
+                                all_schools.append(school_output)
+
+                                print(school_output)
+
+                                time.sleep(0.05)
+
+                            elif city:
+
+                                school_output = {
+                                    'School name': school, 
+                                    'State': state_name,
+                                    'County': county.text,
+                                    'City/town/village': city,
+                                }
+
+                                all_schools.append(school_output)
+
+                                print(school_output)
+
+                                time.sleep(0.05)
+
+                    elif next_element.name == 'h3':
+                        print('next element: ', next_element.name)
+
+                        time.sleep(0.05)
+
+                        city = next_element.text
+
+                    elif next_element.name == 'h2':
+                        break
+
+                    else:
+                        pass
+
+                    next_element = next_element.find_next_sibling()
+
+    return all_schools
+
+
+def SaveData(OP):
+
+    df = pd.DataFrame(OP, columns=['School name', 'State', 'County', 'City/town/village'])
+    df.to_excel('High Schools in the USA.xls', index=True, columns=['School name', 'State', 'County', 'City/town/village'])
+
+
+get_schools(get_state_urls(parent_url))
 
--- tests.py
+++ tests.py
@@ -123,9 +123,64 @@ def get_info2():
     req = requests.get('https://en.wikipedia.org/wiki/List_of_high_schools_in_Alabama')
     soup = BeautifulSoup(req.text, 'html.parser')
 
+    unuseful_links = soup.find_all('link')
+
+    for i in unuseful_links:
+
+        i.decompose()
+
     counties = soup.find_all('h2')
 
-    
+    for county in counties:
+
+        print('+'*20, county.text, '+'*20)
+
+        if 'County' not in county.text:
+
+            pass
+
+        else:
+            
+            if county.find_next_sibling() == county.find_next_sibling('div'):
+
+                print('+'*20 + 'if' + '+'*20)
+
+                school_list_div = county.find_next_sibling('div')
+                schools = school_list_div.find_all('li')
+
+                for i in schools:
+
+                    print(i)
+                    print('')
+
+            else:
+
+                print('+'*20 + 'else' + '+'*20)
+
+                school_list = county.find_next_sibling('ul')
+                schools = school_list.find_all('li')
+
+                for i in schools:
+
+                    print(i)
+                    print('')
+
+
+#get_info2()
+
+
+def get_info3():
+
+    req = requests.get('https://en.wikipedia.org/wiki/List_of_high_schools_in_Alabama')
+    soup = BeautifulSoup(req.text, 'html.parser')
+
+    unuseful_links = soup.find_all('link')
+
+    for i in unuseful_links:
+
+        i.decompose()
+
+    counties = soup.find_all('h2')
 
     for county in counties:
 
@@ -135,20 +190,276 @@ def get_info2():
 
         else:
 
-            school_list = county.find_next_sibling('ul')
-            schools = school_list.find_all('li')
-
-            print('')
-
-            print(county)
-
-            print('')
-
-            print(school_list)
-
-            print('')
-
-
-get_info2()
-
+            print("*"*50 + county.text + "*"*50)
             
+            if county.find_next_sibling() == county.find_next_sibling('h3'):
+
+                print('Case 1: h3 found -- ', county.text)
+
+                city = county.find_next_sibling('h3')
+
+                while True:
+
+                    print('+'*20, city.text, '+'*20)
+
+                    if city.find_next_sibling() == city.find_next_sibling('div'):
+
+                        school_list_div = city.find_next_sibling('div')
+                        schools = school_list_div.find_all('li')
+
+                        for i in schools:
+
+                            print(i)
+                            print('')
+
+                        if school_list_div.find_next_sibling() == school_list_div.find_next_sibling('h3'):
+                            
+                            city = school_list_div.find_next_sibling('h3')
+
+                        elif school_list_div.find_next_sibling() == school_list_div.find_next_sibling('h2'):
+
+                            break                            
+
+                    elif city.find_next_sibling() == city.find_next_sibling('ul'):
+
+                        school_list = city.find_next_sibling('div')
+                        schools = school_list.find_all('li')
+
+                        for i in schools:
+
+                            print(i)
+                            print('')
+
+                        if school_list.find_next_sibling() == school_list.find_next_sibling('h3'):
+                            
+                            city = school_list.find_next_sibling('h3')
+
+                        elif school_list.find_next_sibling() == school_list.find_next_sibling('h2'):
+
+                            break     
+
+            elif county.find_next_sibling() == county.find_next_sibling('div'):
+
+                print('Case 2: div found -- ', county.text)
+
+                school_list_div = county.find_next_sibling('div')
+                schools = school_list_div.find_all('li')
+
+                for i in schools:
+
+                    print(i)
+                    print('')
+
+                if county.find_next_sibling() == county.find_next_sibling('h3'):
+
+                    city = county.find_next_sibling('h3')
+
+                    while True:
+
+                        print('+'*20, city.text, '+'*20)
+
+                        if city.find_next_sibling() == city.find_next_sibling('div'):
+
+                            school_list_div = city.find_next_sibling('div')
+                            schools = school_list_div.find_all('li')
+
+                            for i in schools:
+
+                                print(i)
+                                print('')
+
+                            if school_list_div.find_next_sibling() == school_list_div.find_next_sibling('h3'):
+                                
+                                city = school_list_div.find_next_sibling('h3')
+
+                            elif school_list_div.find_next_sibling() == school_list_div.find_next_sibling('h2'):
+
+                                break                            
+
+                        elif city.find_next_sibling() == city.find_next_sibling('ul'):
+
+                            school_list = city.find_next_sibling('div')
+                            schools = school_list.find_all('li')
+
+                            for i in schools:
+
+                                print(i)
+                                print('')
+
+                            if school_list.find_next_sibling() == school_list.find_next_sibling('h3'):
+                                
+                                city = school_list.find_next_sibling('h3')
+
+                            elif school_list.find_next_sibling() == school_list.find_next_sibling('h2'):
+
+                                break
+                    
+
+            elif county.find_next_sibling() == county.find_next_sibling('ul'):
+
+                print('Case 3: ul found -- ', county.text)
+
+                school_list = county.find_next_sibling('ul')
+                schools = school_list.find_all('li')
+
+                for i in schools:
+
+                    print(i)
+                    print('')
+
+                if county.find_next_sibling() == county.find_next_sibling('h3'):
+
+                    city = county.find_next_sibling('h3')
+
+                    while True:
+
+                        print('+'*20, city.text, '+'*20)
+
+                        if city.find_next_sibling() == city.find_next_sibling('div'):
+
+                            school_list_div = city.find_next_sibling('div')
+                            schools = school_list_div.find_all('li')
+
+                            for i in schools:
+
+                                print(i)
+                                print('')
+
+                            if school_list_div.find_next_sibling() == school_list_div.find_next_sibling('h3'):
+                                
+                                city = school_list_div.find_next_sibling('h3')
+
+                            elif school_list_div.find_next_sibling() == school_list_div.find_next_sibling('h2'):
+
+                                break                            
+
+                        elif city.find_next_sibling() == city.find_next_sibling('ul'):
+
+                            school_list = city.find_next_sibling('div')
+                            schools = school_list.find_all('li')
+
+                            for i in schools:
+
+                                print(i)
+                                print('')
+
+                            if school_list.find_next_sibling() == school_list.find_next_sibling('h3'):
+                                
+                                city = school_list.find_next_sibling('h3')
+
+                            elif school_list.find_next_sibling() == school_list.find_next_sibling('h2'):
+
+                                break
+
+#get_info3()
+
+def get_info4():
+
+    req = requests.get('https://en.wikipedia.org/wiki/List_of_high_schools_in_Alabama')
+    soup = BeautifulSoup(req.text, 'html.parser')
+
+    unuseful_links = soup.find_all('link')
+
+    for i in unuseful_links:
+
+        i.decompose()
+
+    counties = soup.find_all('h2')
+
+    for county in counties:
+
+        if 'County' not in county.text:
+
+            pass
+
+        else:
+
+            print('*'*50, county.text, '*'*50)
+
+            time.sleep(0.5)
+
+            next_element = county.find_next_sibling()
+
+            city = ''
+            school_type = ''
+
+            while True:
+
+                if next_element.name == 'div' or next_element.name == 'ul':
+
+                    print('next element: ', next_element.name)
+
+                    time.sleep(0.5)
+
+                    school_list = next_element.find_all('li')
+                    schools = [school.text for school in school_list]
+
+                    for school in schools:
+
+                        if ', ' in school:
+
+                            school_info = school.split(', ')
+                            name = school_info[0]
+                            city = school_info[1]
+
+                            school_output = {
+                                'School name': name, 
+                                'County': county.text,
+                                'City/town/village': city,
+                                'School type': 'Not specified'
+                            }
+
+                            print(school_output)
+
+                            time.sleep(0.5)
+
+                        elif school_type:
+                            
+                            school_output = {
+                                'School name': school, 
+                                'County': county.text,
+                                'City/town/village': city,
+                                'School type': school_type
+                            }
+
+                            print(school_output)
+
+                            time.sleep(0.5)
+
+                        elif city:
+
+                            school_output = {
+                                'School name': school, 
+                                'County': county.text,
+                                'City/town/village': city,
+                                'School type': 'Not specified'
+                            }
+
+                            print(school_output)
+
+                            time.sleep(0.5)
+
+                elif next_element.name == 'h3':
+                    print('next element: ', next_element.name)
+
+                    time.sleep(0.5)
+
+                    city = next_element.text
+
+                elif next_element.name == 'h4':
+
+                    print('next element: ', next_element.name)
+
+                    time.sleep(0.5)
+
+                    school_type = next_element.text
+
+                elif next_element.name == 'h2':
+                    break
+                else:
+                    pass
+
+                next_element = next_element.find_next_sibling()
+                
+
+get_info4()
